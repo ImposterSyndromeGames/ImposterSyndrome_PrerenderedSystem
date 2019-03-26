a539f5a6daf85eec9d351ccfdb6e7dec5c6c7d7e
ImposterSyndromeGames <48926885+ImposterSyndromeGames@users.noreply.github.com>
2019-03-26 17:39:57 +1100

    Add files via upload

---
+++ Scripts/UniversalMono/ActionBatch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+	[CreateAssetMenu(menuName = "Actions/Mono Actions/Action Batch")]
+	public class ActionBatch : Action
+	{
+		public Action[] actions;
+
+		public override void Execute()
+		{
+			for (int i = 0; i < actions.Length; i++)
+			{
+				actions[i].Execute();
+			}
+		}
+	}
+}
---
+++ Scripts/UniversalMono/DeltaTimeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SO;
+
+namespace SA
+{
+	[CreateAssetMenu(menuName = "Actions/Mono Actions/Delta Time Manager")]
+	public class DeltaTimeManager : Action
+	{
+		public FloatVariable targetVariable;
+
+		public override void Execute()
+		{
+			targetVariable.value = Time.deltaTime;
+		}
+	}
+}
